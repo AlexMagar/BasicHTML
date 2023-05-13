51e447af1e497b7f29e1f20fbdb1660524f26392
AlexMagar <lx.mgr07@gmail.com>
2023-05-13 15:58:36 +1000

    DOM

--- weekSix/js/dom.js
+++ weekSix/js/dom.js
@@ -34,3 +34,36 @@ function clickMe() {
   return (document.getElementById("demo1").innerHTML = "inside the function");
 }
 // clickMe();
+
+// ===== Event listener =====
+// const ele = document.querySelector("#result");
+// ele.addEventListener("click", () => {
+//   //   console.log(ele.innerText);
+//   ele.innerHTML = "Now the text is changed";
+// });
+
+// ========= onload =========
+// function myFunction() {
+//   console.log("Doc loaded");
+// }
+
+// ===== Event listener =====
+const ele = document.querySelector("body");
+ele.addEventListener("click", () => {
+  //   console.log(ele.innerText);
+  ele.innerHTML = "Now the text is changed";
+});
+
+//===== removeEventListener ======
+
+// ========= JSON =======
+{
+  students: [
+    { name: "Jack", age: 23 },
+    { name: "Ram", age: 34 },
+    { name: "Paul", age: 22 },
+  ];
+}
+let studentsData =
+  'students:[ { name: "Jack", age: 23 }, { name: "Ram", age: 34 }]';
+console.log(JSON.parse(studentsData));
