2dcbc46667eeaf93c0d8bc469bfeff6bb204234e
AlexMagar <lx.mgr07@gmail.com>
2023-05-13 23:31:03 +1000

    js code for calculator manipulation

---
+++ calculator/js/script.js
@@ -0,0 +1,13 @@
+// variable;
+const myArr = new Array(() => {
+  document.getElementById("ac").innerHTML;
+});
+console.log(myArr);
+// const result = myArr.push(document.getElementById("result").innerHTML);
+// console.log(result);
+
+function clearResult() {
+  document.getElementById("result").innerHTML;
+  //   const rslt = myArr.push(document.getElementById("ac").innerHTML);
+  console.log("result not publish", rslt);
+}
