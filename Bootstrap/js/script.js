ab71d8dc7b0d3e4d57cce4c9234e3eabaa202a09
AlexMagar <lx.mgr07@gmail.com>
2023-05-19 23:27:37 +1000

    added event listiner

--- Bootstrap/js/script.js
+++ Bootstrap/js/script.js
@@ -1,3 +1,13 @@
 //get the element from the button click
-const btnVl = document.querySelector(".btn").innerHTML;
+const btnVl = document.querySelector(".btn").addEventListener("click", () => {
+  document.querySelector(".itm1").innerHTML = "Hello there";
+});
 console.log(btnVl);
+
+// btnVl.addEventListener("click", () => {
+//   console.log("clicked");
+// });
+
+// if (btnVl === "+ Add New Task") {
+//   console.log("inside the function");
+// }
